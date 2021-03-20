af682c954d8593a334d4c4ac688358e83bdfb6a6
WildUrti <46956225+WildUrti@users.noreply.github.com>
2021-03-20 18:39:12 +0200

    updated lab2

--- Lab2/Domain/Drone.py
+++ Lab2/Domain/Drone.py
@@ -1,32 +1,10 @@
 import pygame
-from pygame.locals import *
 from Drone.Domain.settings import *
 
 
 class Drone:
-    def __init__(self, x, y):
-        self._x = x
-        self._y = y
-        self._visited = {}
-        self._last = None
-        self._stack = [(x, y)]
-        self._path = []
-
-    @property
-    def path(self):
-        return self._path
-
-    @path.setter
-    def path(self, value):
-        self._path = value
-
-    @property
-    def stack(self):
-        return self._stack
-
-    @stack.setter
-    def stack(self, value):
-        self._stack = value
+    def __init__(self, position):
+        self._x, self._y = position
 
     @property
     def position(self):
@@ -36,21 +14,3 @@ class Drone:
     def position(self, value):
         self._x, self._y = value
 
-    @property
-    def visited(self):
-        return self._visited
-
-    @property
-    def last(self):
-        return self._last
-
-    def get_direction(self, position):
-        x, y = position
-        if x < self._x:
-            return UP
-        elif x > self._x:
-            return DOWN
-        elif y < self._y:
-            return LEFT
-        else:
-            return RIGHT
--- Lab2/UI/UI.py
+++ Lab2/UI/UI.py
@@ -1,36 +1,63 @@
 from Drone.Service.Service import *
+from time import sleep
 
 
 class UI:
     def __init__(self):
-        self.service = Service()
-        # self.service.loadEnvironment("Resources/test2.map")
+        self._service = Service()
 
-    def run(self):
         pygame.init()
         logo = pygame.image.load("Resources/logo32x32.png")
         pygame.display.set_icon(logo)
-        pygame.display.set_caption("Resources/drone exploration")
+        pygame.display.set_caption("Path in simple environment")
 
-        screen = pygame.display.set_mode((800, 400))
+        screen = pygame.display.set_mode((SQUARE_WIDTH * WIDTH, SQUARE_HEIGHT * HEIGHT))
         screen.fill(WHITE)
-        screen.blit(self.service.environmentImage, (0, 0))
 
         running = True
-
+        finalPosition = None
+        dronePosition = None
+        printExecutionTimes = False
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-                if event.type == KEYDOWN:
-                    if pygame.key.get_pressed()[K_LEFT]:
-                        self.service.slow_down()
-                    if pygame.key.get_pressed()[K_RIGHT]:
-                        self.service.speed_up()
+                if event.type == pygame.KEYDOWN:
+                    printExecutionTimes = True
+                    if pygame.key.get_pressed()[pygame.K_t]:
+                        self._service.showTabu = not self._service.showTabu
+                    if pygame.key.get_pressed()[pygame.K_s]:
+                        self._service.showSimulatedAnnealing = not self._service.showSimulatedAnnealing
+                    if pygame.key.get_pressed()[pygame.K_c]:
+                        self._service.computeSimulatedAnnealing = not self._service.computeSimulatedAnnealing
+                    if pygame.key.get_pressed()[pygame.K_g]:
+                        self._service.showGreedyAndAStar = not self._service.showGreedyAndAStar
 
-            self.service.moveDFS()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    printExecutionTimes = True
+                    if pygame.mouse.get_pressed()[pygame.BUTTON_LEFT-1] == 1:
+                        y, x = pygame.mouse.get_pos()
+                        finalPosition = (x//SQUARE_HEIGHT, y // SQUARE_WIDTH)
+                    if pygame.mouse.get_pressed()[pygame.BUTTON_RIGHT-1] == 1:
+                        y, x = pygame.mouse.get_pos()
+                        dronePosition = (x//SQUARE_HEIGHT, y // SQUARE_WIDTH)
 
-            screen.blit(self.service.detectedMapImage, (400, 0))
+            if dronePosition is not None:
+                self._service.setDronePosition(dronePosition)
+            screen.blit(self._service.mapWithDrone(), (0, 0))
+            if finalPosition is not None:
+                screen.blit(self._service.mapWithPath(finalPosition), (0, 0))
+
+                if printExecutionTimes:
+                    for strategy, (execution_time, length) in self._service.executionTimes.items():
+                        print(f"{strategy.ljust(10)}: {execution_time} - {length}")
+                    print()
+                    printExecutionTimes = False
+
             pygame.display.flip()
 
+        # pygame.display.flip()
+        # sleep(2)
         pygame.quit()
+
+
